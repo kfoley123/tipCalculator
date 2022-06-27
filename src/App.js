e0701ccc44ed96e4d50f913b2430aa1fc031d13d
Kortney <kortney@iMac.local>
2022-06-27 16:43:17 -0300

    made skeleton for calculator body

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>hello world</h1>
+        <div>
+            <p>Bill Amount</p>
+
+            <input type="text" placeholder="0.00" />
+            <p>Tip Amount</p>
+            <button>10%</button>
+            <button>15%</button>
+            <button>20%</button>
+            <button>Custom</button>
+
+            <p>How many people are paying?</p>
+
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+            <input type="text" placeholder="0.00" />
         </div>
     );
 }
