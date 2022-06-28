8920659e50de12353bd09f505de0257a6b0a4bb2
Kortney <kortney@iMac.local>
2022-06-28 15:18:57 -0300

    hooked up the bill amount, the tip amount and the total with tip to the onChange event.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+    const [billAmount, setBillAmount] = useState(0);
+
+    function getTipAmount(bill, tip) {
+        return parseInt(bill) * (tip / 100);
+    }
+
+    function getTotalwTip(bill, tip) {
+        return getTipAmount(bill, tip) + parseInt(bill);
+    }
+
     return (
         <div className="calculatorContainer">
             <div className="calculatorFunctions">
                 <p>Bill Amount</p>
 
-                <input type="text" placeholder="$0.00" />
+                <input
+                    type="text"
+                    placeholder="$0.00"
+                    onChange={(event) => setBillAmount(event.target.value)}
+                />
+                <div className="tipTotals">
+                    <p>Tip Amount:$ {getTipAmount(billAmount, 15)}</p>
+                    <p>
+                        Total with Tip:$
+                        {getTotalwTip(billAmount, 15)}
+                    </p>
+                </div>
 
                 <div>
                     <div>
@@ -34,7 +56,8 @@ function App() {
             </div>
             <div className="finalOutput">
                 <p>
-                    <span>$0.00</span> / per person
+                    <span>${getTotalwTip(billAmount, 15) / 2}</span> / per
+                    person
                 </p>
             </div>
         </div>
